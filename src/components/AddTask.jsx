5a1b2c8d4bb187cd4ce522c9389217f4b717482a
mateusnaxx <mateus.naxx@gmail.com>
2025-09-02 18:35:49 -0300

    feat: add function handleTaskAdd

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,16 +1,38 @@
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import axios from "axios";
 
 import CustomInput from "./CustomInput";
 import CustomButton from "./CustomButton";
 
 import "./AddTask.scss";
 
-const AddTask = () => {
+const AddTask = ({fetchTasks}) => {
     const [task, setTask] = useState("");
 
     const onChange = (e) => {
         setTask(e.target.value);
+    };
+
+    const handleTaskAdd = async () => {
+        try {
+            if (task.length === 0) {
+                return alert(
+                    "A tarefa precisa de uma descrição para ser adicionada."
+                );
+            }
+
+            await axios.post("http://localhost:8000/tasks", {
+                description: task,
+                is_completed: false,
+            });
+
+            await fetchTasks();
+
+            setTask("");
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -20,8 +42,8 @@ const AddTask = () => {
                 value={task}
                 onChange={onChange}
             />
-            <CustomButton>
-                <FaPlus size={14} color="#fff"/>
+            <CustomButton onClick={handleTaskAdd}>
+                <FaPlus size={14} color="#fff" />
             </CustomButton>
         </div>
     );
