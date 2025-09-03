a568ce287f779ff59de018e38d1128835d83a2cb
mateusnaxx <mateus.naxx@gmail.com>
2025-09-03 11:57:47 -0300

    perf: add last tasks and completed tasks to useMemo

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 
 import "./Tasks.scss";
@@ -19,6 +19,14 @@ const Tasks = () => {
         }
     };
 
+    const lastTasks = useMemo(() => {
+        return tasks.filter((task) => task.isCompleted === false);
+    }, [tasks]);
+
+    const completedTasks = useMemo(() => {
+        return tasks.filter((task) => task.isCompleted === true);
+    }, [tasks]);
+
     useEffect(() => {
         fetchTasks();
     }, []);
@@ -31,10 +39,8 @@ const Tasks = () => {
                 <h3>Últimas Tarefas</h3>
                 <AddTask fetchTasks={fetchTasks} />
                 <div className="tasks-list">
-                    {tasks
-                        .filter((task) => task.isCompleted === false)
-                        .map((lastTask) => (
-                            <TaskItem task={lastTask} fetchTasks={fetchTasks} />
+                    {lastTasks.map((lastTask) => (
+                            <TaskItem key={lastTask._id} task={lastTask} fetchTasks={fetchTasks} />
                         ))}
                 </div>
             </div>
@@ -42,10 +48,9 @@ const Tasks = () => {
             <div className="completed-tasks">
                 <h3>Tarefas Concluídas</h3>
                 <div className="tasks-list">
-                    {tasks
-                        .filter((task) => task.isCompleted)
-                        .map((completedTask) => (
+                    {completedTasks.map((completedTask) => (
                             <TaskItem
+                                key={completedTask._id}
                                 task={completedTask}
                                 fetchTasks={fetchTasks}
                             />
