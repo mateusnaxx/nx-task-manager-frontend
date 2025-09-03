62be3aea9fff15bfa87aa839b36d659066074f0a
mateusnaxx <mateus.naxx@gmail.com>
2025-09-03 16:13:12 -0300

    feat: add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 import "./App.scss";
 
@@ -7,6 +8,7 @@ const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,21 @@
+import { useNavigate } from 'react-router-dom'
+
 import CustomButton from '../components/CustomButton';
-import './Logo.scss'
-import logo from '../assets/logo.png';
+import './Login.scss'
+import logo from '../assets/images/logo.png';
 
 const Login = () => {
+    const navigate = useNavigate()
+
+    const handleLogin = () => {
+        navigate('/')
+    }
+
     return (
         <div className="login-container">
             <img src={logo} alt="NX task manager" />
-            <h1>Login</h1>
             <div className="button-container">
-                <CustomButton>Entrar</CustomButton>
+                <CustomButton onClick={handleLogin}>Entrar</CustomButton>
             </div>
         </div>
     );
